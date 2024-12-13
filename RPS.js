51ea25a603d7273038e86d08567ffe6b361cd8d4
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-12 19:54:51 -0500

    refactoring a bit

--- RPS.js
+++ RPS.js
@@ -2,6 +2,8 @@
 
 //use an array to store all possible choices
 const choices = ["rock", "paper", "scissors"];
+const c_choice = choices[getComputerChoice()];
+const u_choice = getUserChoice();
 
 //get the computers choice
 function getComputerChoice() {
@@ -9,25 +11,44 @@ function getComputerChoice() {
     return c_choice;
 }
 
-let c_choice = choices[getComputerChoice()];
-
 //get users choice
 function getUserChoice() {
     let u_choice = prompt("Please type rock, paper, or scissors").toLowerCase();
     return u_choice;
 }
 
-let u_choice = getUserChoice();
-
 //conditional to ensure the user can only enter one of the choices
-while (!choices.includes(u_choice)) {
-    alert("Please only type rock, paper, or scissors...");
-    u_choice = getUserChoice();      
-}
+
 
 console.log("RESULTS");
 
-//conditionals for scenarios
-if (c_choice == u_choice) {
-    console.log("It's a tie!");
+//define a count for games played and player scores
+let count = 1;
+let c_score = 0;
+let u_score = 0;
+
+while (count <= 5) {
+    if (c_choice == u_choice) {
+        console.log("It's a tie!");
+        continue
+    }
+
+    if (c_choice == "rock" && u_choice == "paper") {
+        u_score++;
+        alert(`You take this round!\n SCORE:\n You: ${u_score}\n Opponent: ${c_score}`);
+    } else if (c_choice == "rock" && u_choice == "scissors") {
+        c_score++;
+        alert(`You lost this round!\n SCORE:\n You: ${u_score}\n Opponent: ${c_score}`);
+    } else if (c_choice == "paper" && u_choice == "scissors") {
+        u_score++;
+        alert(`You take this round!\n SCORE:\n You: ${u_score}\n Opponent: ${c_score}`);
+    } 
 }
+
+function playRound() {
+    while (!choices.includes(u_choice)) {
+        alert("Please only type rock, paper, or scissors...");
+        u_choice = getUserChoice();      
+    }
+}
+//conditionals for scenarios
