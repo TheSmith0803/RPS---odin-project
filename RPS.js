959085b7b6a3ede109daa8b71035deeb1be3ed59
TheSmith0803 <ethan.smith@ametek.com>
2024-12-13 08:47:32 -0500

    finished game logic and added win/lose message

--- RPS.js
+++ RPS.js
@@ -1,54 +1,79 @@
 /**ROCK PAPER SCISSORS*/
 
-//use an array to store all possible choices
+//use an array to store all possible choices and count for round number
 const choices = ["rock", "paper", "scissors"];
-const c_choice = choices[getComputerChoice()];
-const u_choice = getUserChoice();
+let count = 1;
+let c_choice;
+let u_choice;
 
-//get the computers choice
 function getComputerChoice() {
     let c_choice = Math.floor(Math.random() * 3);
     return c_choice;
 }
 
-//get users choice
 function getUserChoice() {
     let u_choice = prompt("Please type rock, paper, or scissors").toLowerCase();
     return u_choice;
 }
 
-//conditional to ensure the user can only enter one of the choices
+function playRound() {
+    while (!choices.includes(u_choice)) {
+        alert("Please only type rock, paper, or scissors...");
+        u_choice = getUserChoice();
+    }
 
+    if (u_choice == c_choice) {
+        alert(`It's a tie!\nSCORE:\nYou: ${u_score}\nOpponenet: ${c_score}`);
+        count--;
+    }
 
-console.log("RESULTS");
+    if (u_choice == "rock" && c_choice == "scissors") {
+        u_score++;
+        alert(`You took this round!\nSCORE:\nYou: ${u_score}\nOpponenet: ${c_score}`);
+    } else if (u_choice == "rock" && c_choice == "paper") {
+        c_score++;
+        alert(`Opponent took this round!\nSCORE:\nYou: ${u_score}\nOpponenet: ${c_score}`);
+    }
+
+    if (u_choice == "paper" && c_choice == "rock") {
+        u_score++;
+        alert(`You took this round!\nSCORE:\nYou: ${u_score}\nOpponenet: ${c_score}`);
+    } else if (u_choice == "paper" && c_choice == "scissors") {
+        c_score++;
+        alert(`Opponent took this round!\nSCORE:\nYou: ${u_score}\nOpponenet: ${c_score}`);
+    }
+
+    if (u_choice == "scissors" && c_choice == "paper") {
+        u_score++;
+        alert(`You took this round!\nSCORE:\nYou: ${u_score}\nOpponenet: ${c_score}`);
+    } else if (u_choice == "scissors" && c_choice == "rock") {
+        c_score++;
+        alert(`Opponent took this round!\nSCORE:\nYou: ${u_score}\nOpponenet: ${c_score}`);
+    }
+
+    count++;
+}
 
 //define a count for games played and player scores
-let count = 1;
 let c_score = 0;
 let u_score = 0;
 
 while (count <= 5) {
-    if (c_choice == u_choice) {
-        console.log("It's a tie!");
-        continue
-    }
+    alert(`Current round: ${count}`);
+    c_choice = choices[getComputerChoice()];
+    u_choice = getUserChoice();
+    let result = playRound();
+    console.log(result);
 
-    if (c_choice == "rock" && u_choice == "paper") {
-        u_score++;
-        alert(`You take this round!\n SCORE:\n You: ${u_score}\n Opponent: ${c_score}`);
-    } else if (c_choice == "rock" && u_choice == "scissors") {
-        c_score++;
-        alert(`You lost this round!\n SCORE:\n You: ${u_score}\n Opponent: ${c_score}`);
-    } else if (c_choice == "paper" && u_choice == "scissors") {
-        u_score++;
-        alert(`You take this round!\n SCORE:\n You: ${u_score}\n Opponent: ${c_score}`);
-    } 
-}
-
-function playRound() {
-    while (!choices.includes(u_choice)) {
-        alert("Please only type rock, paper, or scissors...");
-        u_choice = getUserChoice();      
+    if (count > 5) {
+        break;
     }
 }
+
+if (u_score == 3 || u_score > c_score) {
+    alert("You win!!");
+} else if (c_score == 3 || c_choice > u_choice) {
+    alert("You lose!");
+}
+
 //conditionals for scenarios
