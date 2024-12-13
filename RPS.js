1ca9f25b51b72d93d6c7e04830fbd8ea5bc864ec
TheSmith0803 <ethan.smith@ametek.com>
2024-12-13 09:10:57 -0500

    trying to add option to play multiple times

--- RPS.js
+++ RPS.js
@@ -60,21 +60,41 @@ let c_score = 0;
 let u_score = 0;
 
 //game loop
-while (count <= 5) {
-    alert(`Current round: ${count}`);
-    c_choice = choices[getComputerChoice()];
-    u_choice = getUserChoice();
-    let result = playRound();
-    console.log(result);
+function playGame() {
+    while (count <= 5) {
+        alert(`Current round: ${count}`);
+        c_choice = choices[getComputerChoice()];
+        u_choice = getUserChoice();
+        let result = playRound();
+        console.log(result);
 
-    if (count > 5) {
-        break;
+        if (count > 5) {
+            break;
+        }
+    }
+
+    //win-lose messages
+    if (u_score == 3 || u_score > c_score) {
+        alert("You win!!");
+    } else if (c_score == 3 || c_choice > u_choice) {
+        alert("You lose!");
     }
 }
 
-//win-lose messages
-if (u_score == 3 || u_score > c_score) {
-    alert("You win!!");
-} else if (c_score == 3 || c_choice > u_choice) {
-    alert("You lose!");
+playGame();
+
+let play_again = prompt("Would you like to play again? (type y or n)").toLowerCase();
+
+while (play_again) {
+    if (play_again == "y") {
+        u_score = 0;
+        c_score = 0;
+        count = 1;
+        playGame();
+    } else if (play_again == "n") {
+        alert("poop");
+        break
+    } else {
+        alert("nah fam");
+    }
 }
