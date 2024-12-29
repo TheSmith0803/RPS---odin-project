5d4126370349b51f8e9da5083302e8ced51e0ca4
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-29 11:31:58 -0500

    encapsulated buttons into 1 event listener and added logic

--- RPS.js
+++ RPS.js
@@ -2,81 +2,94 @@
 
 //use an array to store all possible choices and count for round number
 //init variables for computer and user choices
-const choices = ["rock", "paper", "scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 let count = 0;
+let humanScore = 0;
+let computerScore = 0;
 
+//get a random computer choice and return string from choices array
 function getComputerChoice() {
-    let c_choice = Math.floor(Math.random() * 3);
+    let c_num = Math.floor(Math.random() * 3);
+    let c_choice = choices[c_num];
     return c_choice;
 }
 
-function getUserChoice() {
-    
-    return u_choice;
-}
-
 //variables for each button
-const rockBtn = document.querySelector("#rock-button");
-const paperBtn = document.querySelector("#paper-button");
-const scissorBtn =document.querySelector("#scissors-button");
+const buttons = document.querySelectorAll(".button");
+let buttons_arr = Array.from(buttons);
+console.log(buttons_arr[0].textContent);
 
 //variable for rounds played and displaying choices
 const rounds = document.querySelector("#count");
 const results = document.querySelector("#results");
+
 //REMOVE! makes results ALWAYS visible
 //results.style["color"] = "#000";
 
-//event listeners for button interaction. I know...DRY but i feel like this isn't TOO bad
-rockBtn.addEventListener("click", () => {
-    let u_choice = "rock";
-    let c_choice = choices[getComputerChoice()];
-    count++;
-    rounds.textContent = `Rounds played: ${count}`;
-    results.style["color"] = "#000";
+//loop to handle each buttonn
+for (let i = 0; i < buttons_arr.length; i++) {
 
-    if (u_choice === c_choice) {
-        results.textContent = `You both chose ${c_choice}, it's a tie!`;
-    } 
-    else if (c_choice === "paper") {
-        results.textContent = `Opponent chose ${c_choice}, you lose!`
-    } else {
-        results.textContent = `Opponent chose ${c_choice}, you win!`
-    }
-});
+    //event listener for button interaction.
+    buttons_arr[i].addEventListener("click", () => {
 
-paperBtn.addEventListener("click", () => {
-    let u_choice = "paper";
-    let c_choice = choices[getComputerChoice()];
-    count++;
-    rounds.textContent = `Rounds played: ${count}`;
-    results.style["color"] = "#000";
+        let u_choice = buttons_arr[i].textContent.toLowerCase();
+        let c_choice = getComputerChoice().toLowerCase();
 
-    if (u_choice === c_choice) {
-        results.textContent = `You both chose ${c_choice}, it's a tie!`;
-    } 
-    else if (c_choice === "scissors") {
-        results.textContent = `Opponent chose ${c_choice}, you lose!`
-    } else {
-        results.textContent = `Opponent chose ${c_choice}, you win!`
-    }
-});
+        console.log("User Choice: " + u_choice);
+        console.log("Computer Choice: " + c_choice);
 
-scissorBtn.addEventListener("click", () => {
-    let u_choice = "scissors";
-    let c_choice = choices[getComputerChoice()];
-    count++;
-    rounds.textContent = `Rounds played: ${count}`;
-    results.style["color"] = "#000";
+        //conditional to properly increment count
+        if (u_choice !== c_choice) {
+            count++;
+        }
 
-    if (u_choice === c_choice) {
-        results.textContent = `You both chose ${c_choice}, it's a tie!`;
-    } 
-    else if (c_choice === "rock") {
-        results.textContent = `SMASH! Opponent chose ${c_choice}, you lose!`
-    } else {
-        results.textContent = `Opponent chose ${c_choice}, you win!`
-    }
-});
+        rounds.textContent = `Rounds played: ${count}`;
+        results.style["color"] = "#000";
+        
+        
+        //conditional for tie
+        if (u_choice === c_choice) {
+            results.textContent = `You both chose ${c_choice}, it's a tie!`;
+        } 
+        //conditionals for rock
+        else if (u_choice === "rock" && c_choice === "scissors") {
+            results.textContent = `You chose ${u_choice}, opponent chose ${c_choice}, you win!`;
+            humanScore++;
+        } 
+        //conditional for paper
+        else if (u_choice === "paper" && c_choice === "rock") {
+            results.textContent = `You chose ${u_choice}, opponent chose ${c_choice}, you win!`;
+            humanScore++;
+        }
+        //conditional for scissors
+        else if (u_choice === "scissors" && c_choice === "paper") {
+            results.textContent = `You chose ${u_choice}, opponent chose ${c_choice}, you win!`;
+            humanScore++;
+        }
+        //conditional for losing
+        else {
+            results.textContent = `You chose ${u_choice}, opponent chose ${c_choice}, you lose!`;
+            computerScore++;
+        }
+
+        console.log("HS: " + humanScore);
+        console.log("CS: " + computerScore);
+
+        //conditional for extra text at end of game
+        if (count >= 5) {
+            results.innerHTML = "<div><br>Would you like play again?</div>";
+        }
+    });
+}
+
+while (count >= 5) {
+    results.textContent += "?????"
+}
+
+
+
+
+
 
 //console.log(u_choice);
 
